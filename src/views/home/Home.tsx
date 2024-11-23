03efd86b9167951bacf84570a8a97e3851161679
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2024-11-23 00:29:54 -0300

    feat: implement i18n internationalization feature

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,9 +1,21 @@
+import { useTranslation } from "react-i18next";
 import "./Home.scss";
 
 const Home = () => {
+  const { t } = useTranslation();
+
   return (
     <main className="main">
-      <h1>Home</h1>
+      <div>
+        <h1>{t("name")}</h1>
+        <h3>{t("jobTitle")}</h3>
+
+        <div>
+          <button>Github</button>
+          <button>Linkedin</button>
+          <button>Gmail</button>
+        </div>
+      </div>
     </main>
   );
 };
