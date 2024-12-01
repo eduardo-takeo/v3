ebd826ff8ee7bf71cc89fb640b884d641890d653
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2024-12-01 18:57:03 -0300

    feat: create mouse glow effect

---
+++ src/helpers/mouseMoveHandler.js
@@ -0,0 +1,6 @@
+// src/index.js
+document.addEventListener("mousemove", (e) => {
+  const root = document.documentElement;
+  root.style.setProperty("--mouse-x", `${e.clientX}px`);
+  root.style.setProperty("--mouse-y", `${e.clientY}px`);
+});
